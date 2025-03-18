a49637134f9fffdfc5472b589fab83fc1d0e153b
simfrisk <simoncarlfrisk@gmail.com>
2025-03-18 16:23:52 +0100

    updated the time from unix time to normal

--- dist/temp-script.js
+++ dist/temp-script.js
@@ -1,11 +1,16 @@
 // DOM Elements
 const dailyForecast = document.getElementById("daily-forecast");
 
-//Fetch API
+let data = []
 
-//Base URL
-//Key
-//Cordinates
+//refactor const
+
+// Convert Unix timestamp to readable time
+const formatTime = (timestamp) => {
+  const date = new Date(timestamp * 1000)
+  return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+}
+
 
 
 const baseURL = "https://api.openweathermap.org/data/2.5/"
@@ -22,11 +27,11 @@ const fetchWeather = () => {
     .then((data) => {
       console.log("weather data", data.main.temp)
       dailyForecast.innerHTML = `
-      <h1 class="current-temp">${data.main.temp}</h1>
+      <h1 class="current-temp">${data.main.temp.toFixed(0)}</h1>
       <h2 class="city">${data.name}</h2>
       <h3 class="weather-description">${data.weather[0].description}</h3>
-      <h3 class="sunrise">${data.sys.sunrise}</h3>
-      <h3 class="sunset">${data.sys.sunset}</h3>
+      <h3 class="sunrise">${formatTime(data.sys.sunrise)}</h3>
+      <h3 class="sunset">${formatTime(data.sys.sunset)}</h3>
       `
     })
 }
