72bc3d76184d3fe620bd2f6512e45225a5475745
Ida Hellgren <idakatrinhellgren@gmail.com>
2025-03-19 10:34:34 +0100

    Added a function to fetch the forecast data

--- dist/temp-script.js
+++ dist/temp-script.js
@@ -1,5 +1,6 @@
 // DOM Elements
-const dailyForecast = document.getElementById("daily-forecast");
+const dailyForecast = document.getElementById("daily-forecast")
+const weeklyForecast = document.getElementById("weekly-forecast")
 
 //Fetch API
 
@@ -11,8 +12,12 @@ const dailyForecast = document.getElementById("daily-forecast");
 const baseURL = "https://api.openweathermap.org/data/2.5/"
 const apiKEY = "dfd2a92cf6c2789182807260f210958f"
 const apiURL = `${baseURL}weather?q=Stockholm&units=metric&APPID=${apiKEY}`
+let cityName = "Stockholm"
+
+const apiURLForecast = `${baseURL}forecast?q=${cityName}&units=metric&appid=${apiKEY}`
 
 console.log(`${apiURL} works`)
+console.log(`${apiURLForecast} works`)
 
 
 //Generete elements
@@ -31,7 +36,32 @@ const fetchWeather = () => {
     })
 }
 
+const fetchForecast = () => {
+  fetch(apiURLForecast)
+    .then(response => response.json())
+    .then((data) => {
+      console.log("forecast data", data)
+      const forecastList = data.list
+      .filter((forecast) => forecast.dt_txt.endsWith("12:00:00"))
+      .slice(1, 5)
+      console.log(forecastList)
+      forecastList.forEach((forecast) => {
+        const date = new Date(forecast.dt * 1000)
+        const forecastDay = date.toLocaleDateString("en-US", { weekday: "short" })
+        weeklyForecast.innerHTML += `
+        <li>
+        <p>${forecastDay}</p>
+        <p>${forecast.weather[0].main}</p>
+        <p>${Math.round(forecast.main.temp)}°C</p>
+        </li>`
+      })
+    }
+    )
+}
+
 fetchWeather()
+
+fetchForecast()
 
 
 
