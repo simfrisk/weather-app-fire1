10bfe6fd878cd82764e223924e999cb34f741821
simfrisk <simoncarlfrisk@gmail.com>
2025-03-21 15:12:06 +0100

    Finished the typescript and made the code cleaner

--- dist/script.js
+++ dist/script.js
@@ -8,6 +8,7 @@ const searchBtn = document.getElementById("search-btn");
 const weeklyForecast = document.getElementById("weekly-forecast");
 const forecastIcon = document.getElementById("forecast-icon");
 const forecastDiv = document.getElementById("forecast");
+const showForecastBtn = document.getElementById("toggle-btn");
 const body = document.body;
 let data = [];
 //Convert Unix timestamp to readable time with timezone
@@ -53,15 +54,156 @@ const fetchWeather = (city = "Stockholm", lat, lon) => {
     }
     fetch(apiURL)
         .then((response) => {
-        if (!response.ok)
-            throw new Error("The city was not found!");
-        return response.json();
-    })
+            if (!response.ok)
+                throw new Error("The city was not found!");
+            return response.json();
+        })
         .then((data) => {
-        dayForecast(data);
-        updateBackground(Date.now() / 1000, data.sys.sunrise, data.sys.sunset);
-    })
+            dayForecast(data);
+            updateBackground(Date.now() / 1000, data.sys.sunrise, data.sys.sunset);
+        })
         .catch(() => {
-        dailyForecast.innerHTML = `<p>Sorry, we have no weather data matching your search, please select another city.</p>`;
-    });
+            dailyForecast.innerHTML = `<p>Sorry, we have no weather data matching your search, please select another city.</p>`;
+        });
 };
+const dayForecast = (data) => {
+    const iconCode = data.weather[0].icon;
+    const weather = weatherIcons[iconCode];
+    const iconUrl = `./assets/icons/${weather.file}`;
+    const description = capitalFirst(data.weather[0].main);
+    const timezoneOffset = data.timezone;
+    dailyForecast.innerHTML = `
+  <div class="top-forecast">
+    <h1 class="current-temp">${data.main.temp.toFixed(0)}<sup>°C</sup></h1>
+    <h2 class="city">${data.name}</h2>
+    <h3 class="weather-description">${description}</h3>
+  </div>
+  <div id="sun-position" class="sun-position">
+    <h3 class="sunrise">Sunrise ${formatTime(data.sys.sunrise, timezoneOffset)}</h3>
+    <h3 class="sunset">Sunset ${formatTime(data.sys.sunset, timezoneOffset)}</h3>
+  </div>
+`;
+    forecastIcon.innerHTML = `<img src="${iconUrl}" alt="${description}">`;
+};
+const fetchForecast = (city = "Stockholm", lat, lon) => {
+    let apiURLForecast;
+    if (lat !== undefined && lon !== undefined) {
+        apiURLForecast = `${baseURL}forecast?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKEY}`;
+    }
+    else {
+        apiURLForecast = `${baseURL}forecast?q=${city}&units=metric&APPID=${apiKEY}`;
+    }
+    fetch(apiURLForecast)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error("The city was not found!");
+            return response.json();
+        })
+        .then((data) => {
+            const timezoneOffset = data.city.timezone;
+            const forecastList = data.list
+                .filter((forecast) => forecast.dt_txt.endsWith("12:00:00"))
+                .slice(0, 4);
+            weeklyForecast.innerHTML = "";
+            forecastList.forEach((forecast) => {
+                const date = new Date(forecast.dt * 1000);
+                const forecastDay = date.toLocaleDateString("en-US", { weekday: "short" });
+                const iconCode = forecast.weather[0].icon;
+                const weather = weatherIcons[iconCode];
+                const iconUrl = `./assets/icons/${weather.file}`;
+                weeklyForecast.innerHTML += `
+          <li>
+            <p>${forecastDay}</p>
+            <div class="weather-temp">
+              <img src="${iconUrl}" alt="${forecast.weather[0].description}">
+              <p>${Math.round(forecast.main.temp)}°C</p>
+            </div>
+          </li>`;
+            });
+            weeklyForecast.style.display = "none";
+            weeklyForecast.classList.add("toggle-forecast-hide");
+        })
+        .catch((error) => {
+            weeklyForecast.innerHTML = `<p>${error.message}</p>`;
+        });
+};
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+    else {
+        alert("Geolocation is not supported by this browser.");
+        fetchWeather();
+        fetchForecast();
+    }
+};
+const showPosition = (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    fetchWeather(undefined, lat, lon);
+    fetchForecast(undefined, lat, lon);
+};
+const showError = (error) => {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+    }
+    fetchWeather();
+    fetchForecast();
+};
+searchBtn.addEventListener("click", () => {
+    const city = cityInput === null || cityInput === void 0 ? void 0 : cityInput.value.trim();
+    if (city) {
+        fetchWeather(city);
+        fetchForecast(city);
+    }
+    else {
+        alert("Try again with a valid city");
+    }
+});
+getLocation();
+const updateBackground = (currentTime, sunrise, sunset) => {
+    if (currentTime >= sunrise && currentTime < sunset) {
+        body.classList.remove("night");
+    }
+    else {
+        body.classList.add("night");
+    }
+};
+showForecastBtn.addEventListener("click", () => {
+    showForecastBtn.disabled = true;
+    const sunPosition = document.getElementById("sun-position");
+    if (!weeklyForecast.classList.contains("toggle-forecast-show")) {
+        weeklyForecast.style.display = "block";
+        setTimeout(() => {
+            forecastDiv.classList.add("compact");
+            sunPosition.classList.add("compact-sun");
+            weeklyForecast.classList.add("toggle-forecast-show");
+            showForecastBtn.classList.add("btn-shift");
+        }, 5);
+        setTimeout(() => {
+            showForecastBtn.textContent = "▼";
+            showForecastBtn.disabled = false;
+        }, 600);
+    }
+    else {
+        weeklyForecast.classList.remove("toggle-forecast-show");
+        forecastDiv.classList.remove("compact");
+        sunPosition.classList.remove("compact-sun");
+        showForecastBtn.classList.remove("btn-shift");
+        setTimeout(() => {
+            weeklyForecast.style.display = "none";
+        }, 300);
+        setTimeout(() => {
+            showForecastBtn.textContent = "▲";
+            showForecastBtn.disabled = false;
+        }, 300);
+    }
+});
